eb093b8e6506d810201c41ea9204cb888a884e76
IgorIA <igorsemphoski@gmail.com>
2022-05-05 00:00:48 -0300

    feat: stage 2

---
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,31 @@
+import { Fragment } from "react";
+import { CloseButton } from "../../CloseButton";
+
+import successImgUrl from "../../../assets/success.svg";
+
+interface FeedbackSuccessStepProps {
+  onFeedbackRestartRequested: () => void;
+}
+
+export function FeedbackSuccessStep({
+  onFeedbackRestartRequested
+} : FeedbackSuccessStepProps) {
+  return (
+    <Fragment>
+      <header>
+        <CloseButton />
+      </header>
+      <div className="flex flex-col items-center py-10 w-[304px]">
+        <img src={successImgUrl} alt="Success icon" />
+        <span className="text-xl mt-2">Agradecemos o feedback!</span>
+        <button 
+          className="py-2 px-6 mt-6 bg-zinc-600 rounded-md border-transparent text-sm leading-6 hover:bg-zinc-700 transition-colors focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-zinc-900 focus:ring-brand-500"
+          type="button"
+          onClick={onFeedbackRestartRequested}
+          >
+            Quero enviar outro
+          </button>
+      </div>
+    </Fragment>
+  );
+}
