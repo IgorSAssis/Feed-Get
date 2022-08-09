5c68f679bcbefd3eb529a10d1f9b94ee156bb999
IgorSAssis <igorsemphoski@gmail.com>
2022-08-09 12:50:07 -0300

    fix: fix page overflow

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -5,7 +5,7 @@ import { Widget } from "./components/Widget";
 
 export function App() {
   return (
-    <div className="w-screen h-screen pt-14">
+    <div className="w-[100%] h-screen pt-14">
       <Header />
       <main className="w-[90%] m-auto pt-8 pb-8 gap-4 grid grid-cols-feedbacks">
         <Feedback />
