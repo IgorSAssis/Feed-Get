9846b4f2636d57dd306cc6dbf616803779d71c28
IgorIA <igorsemphoski@gmail.com>
2022-08-14 23:44:54 -0300

    feat: fetch feedback list from backend

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,26 +1,40 @@
-import { Feedback } from "./components/Feedback";
+import { useEffect, useState } from "react";
+import { Feedback, FeedbackProps } from "./components/Feedback";
 import { Header } from "./components/Header";
 import { Widget } from "./components/Widget";
+import { api } from "./libs/api";
 
 export function App() {
+  const [feedbacks, setFeedbacks] = useState<FeedbackProps[]>([]);
+
+  useEffect(() => {
+    api
+      .get("/feedbacks")
+      .then((response) => {
+        const feedbacks: FeedbackProps[] = response.data.feedbacks;
+
+        setFeedbacks(feedbacks);
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  }, []);
+
   return (
     <div className="w-[100%] h-screen pt-14">
       <Header />
       <main className="w-[90%] m-auto pt-8 pb-8 gap-4 grid grid-cols-feedbacks">
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
-        <Feedback />
+        {feedbacks.map(({ id, type, comment, screenshot }) => {
+          return (
+            <Feedback
+              key={id}
+              id={id}
+              type={type}
+              comment={comment}
+              screenshot={screenshot}
+            />
+          );
+        })}
       </main>
       <Widget />
     </div>
--- web/src/components/Feedback.tsx
+++ web/src/components/Feedback.tsx
@@ -1,32 +1,78 @@
-import bugImage from "../assets/bug.svg";
+import { Fragment, useState } from "react";
+import { Dialog } from "@headlessui/react";
+import { X } from "phosphor-react";
 
-export function Feedback() {
+import { feedbackTypes } from "./WidgetForm";
+
+export interface FeedbackProps {
+  id?: string;
+  type: "BUG" | "IDEA" | "OTHER";
+  comment: string;
+  screenshot: null | string;
+}
+
+export function Feedback({ id, type, comment, screenshot }: FeedbackProps) {
+  const feedbackType = feedbackTypes[type];
+  const [isOpen, setIsOpen] = useState(false);
 
   return (
-    <div 
-      className="bg-light-surfaceSecondary dark:bg-surfacePrimary rounded-xl overflow-hidden flex flex-col items-center p-4 h-[280px]"
-    >
-      <header className="flex items-center gap-2"> 
-        <img 
-          src={bugImage} 
-          alt="type" 
-          className="w-8 h-8"
-        />
-        <span className="text-xl">Problema</span>
-      </header>
-      <div className="flex flex-col items-center gap-3 h-full justify-around">
-        <div>
-          <label htmlFor="comment">Comentário</label>
-          <textarea 
-            id="comment"
-            className="w-full resize-y border-light-stroke rounded-lg dark:bg-surfacePrimary dark:border-stroke max-h-[110px] min-h-[90px]"
-          ></textarea>
+    <Fragment>
+      <div className="bg-light-surfaceSecondary dark:bg-surfacePrimary rounded-xl overflow-hidden flex flex-col items-center p-4 h-[280px]">
+        <header className="flex items-center gap-2">
+          <img
+            src={feedbackType.image.source}
+            alt={feedbackType.image.alt}
+            className="w-8 h-8"
+          />
+          <span className="text-xl">{feedbackType.title}</span>
+        </header>
+        <div className="flex flex-col items-center gap-3 h-full justify-around">
+          <div>
+            <label htmlFor={id}>Comentário</label>
+            <textarea
+              id={id}
+              className="w-full resize-y border-light-stroke rounded-lg dark:bg-surfacePrimary dark:border-stroke max-h-[110px] min-h-[90px]"
+              value={comment}
+              readOnly
+              disabled
+            ></textarea>
+          </div>
+          <button
+            className="bg-brand-500 text-light-surfacePrimary py-[10px] px-5 rounded-xl w-[80%]"
+            onClick={() => setIsOpen(true)}
+          >
+            Screenshot
+          </button>
         </div>
-        <button
-          className="bg-brand-500 text-light-surfacePrimary py-[10px] px-5 rounded-xl w-[80%]"
-        >Screenshot</button>
       </div>
-    </div>
-  );
+      {isOpen && (
+        <Dialog
+          open={isOpen}
+          onClose={() => setIsOpen(false)}
+          className={`absolute top-[50%] left-[50%] translate-x-[-50%] translate-y-[-50%] z-10 bg-light-surfaceSecondary border-[1px] dark:bg-surfaceSecondary border-gray-200 min-w-[70%] ${
+            !screenshot && "min-h-[40%]"
+          } rounded-xl flex justify-center items-center`}
+        >
+          <Dialog.Panel>
+            {screenshot ? (
+              <img
+                src={screenshot}
+                alt="Feedback screenshot"
+                className="w-full h-full"
+              />
+            ) : (
+              <span className="text-2xl">Nenhuma screenshot associada!</span>
+            )}
 
+            <button
+              onClick={() => setIsOpen(false)}
+              className="absolute left-[50%] top-[-40px] translate-x-[-50%] bg-red-500 p-[5px] w-[100px] rounded-t-3xl flex align-middle justify-center"
+            >
+              <X size={30} color="#FFF" className="" />
+            </button>
+          </Dialog.Panel>
+        </Dialog>
+      )}
+    </Fragment>
+  );
 }
