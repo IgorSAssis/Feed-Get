ac68234d431a86d010c8d115e5581e1ca6864afa
IgorIA <igorsemphoski@gmail.com>
2022-08-14 23:48:01 -0300

    feat: create error card

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Feedback, FeedbackProps } from "./components/Feedback";
 import { Header } from "./components/Header";
 import { Widget } from "./components/Widget";
@@ -6,6 +6,7 @@ import { api } from "./libs/api";
 
 export function App() {
   const [feedbacks, setFeedbacks] = useState<FeedbackProps[]>([]);
+  const toastErrorElement = useRef<HTMLDivElement>(null);
 
   useEffect(() => {
     api
@@ -17,13 +18,23 @@ export function App() {
       })
       .catch((error) => {
         console.error(error);
+
+        if (toastErrorElement.current) {
+          toastErrorElement.current.style.right = "20px";
+
+          setTimeout(() => {
+            if (toastErrorElement.current) {
+              toastErrorElement.current.style.right = "-500px";
+            }
+          }, 2000);
+        }
       });
   }, []);
 
   return (
     <div className="w-[100%] h-screen pt-14">
       <Header />
-      <main className="w-[90%] m-auto pt-8 pb-8 gap-4 grid grid-cols-feedbacks">
+      <main className="w-[90%] m-auto pt-8 pb-8 gap-4 grid grid-cols-feedbacks overflow-x-hidden">
         {feedbacks.map(({ id, type, comment, screenshot }) => {
           return (
             <Feedback
@@ -35,6 +46,18 @@ export function App() {
             />
           );
         })}
+        <div
+          ref={toastErrorElement}
+          className="absolute right-[-500px] top-20 bg-red-400 w-fit py-3 px-5 rounded-lg transition-[right] duration-1000 ease-in-out"
+        >
+          <span className="block text-xl w-[100%] text-center text-white font-bold mb-2">
+            Houve algum erro!
+          </span>
+          <p className="text-white text-lg">
+            Não foi possível carregar a listagem de feedbacks.
+            <br /> Tente novamente mais tarde.
+          </p>
+        </div>
       </main>
       <Widget />
     </div>
