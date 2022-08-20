68e0adba81ca771b894c96d9466840b3e99d046e
IgorIA <igorsemphoski@gmail.com>
2022-08-19 21:07:50 -0300

    fix: change allowed request body size

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ const app_port = process.env.PORT || 3333;
 const app = express();
 
 app.use(cors()); 
-app.use(express.json());
+app.use(express.json({ limit: "50mb" }));
 app.use(routes);
 
 app.listen(app_port, () => console.log(`HTTP server running!`));
